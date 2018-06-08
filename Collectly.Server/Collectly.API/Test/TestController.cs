41bca6f933e645ce1735b9c44fb568ef0aaeac93
Leverson Carlos <lcjohnny@hotmail.com>
2018-06-08 09:13:48 -0300

    Client Credentials

---
+++ Collectly.Server/Collectly.API/Test/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Collectly.API.Test
+{
+
+   [Route("[controller]")]
+   [Authorize]
+   public class TestController : ControllerBase
+   {
+
+      [HttpGet]
+      public IActionResult Get()
+      {
+         return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+      }
+
+   }
+
+}
