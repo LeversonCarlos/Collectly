fcf5204f8bd153ddd8c3bf3d386680fbc7e0d2cf
Leverson Carlos <lcjohnny@hotmail.com>
2020-02-17 19:28:40 -0300

    wip

--- legacy/Collectly.Identity/Auth/Data/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Collectly.Auth.Data
-{
-   public class Role : IdentityRole
-   {
-
-   }
-}
--- src/Collectly.API/Auth/Store/RoleData.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Collectly.Auth.Store
-{
-   internal class RoleData : IdentityRole<string>
-   {
-
-   }
-}
