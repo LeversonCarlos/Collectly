45d89a0fc3b45f15ba9ecc63262116ca3731692b
Leverson Carlos <lcjohnny@hotmail.com>
2020-02-17 20:55:43 -0300

    Initializing a new aspnet core backend project with an angular frontend app (#6)

---
+++ sources/Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Collectly
+{
+   public partial class Startup
+   {
+
+      public IConfiguration Configuration { get; }
+      public Startup(IConfiguration configuration)
+      {
+         this.Configuration = configuration;
+      }
+
+      public void ConfigureServices(IServiceCollection services)
+      {
+         this.AddSettings(services);
+         this.AddLocalization(services);
+         this.AddServices(services);
+         this.AddControllers(services);
+      }
+
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         this.UseSettings(app, env);
+         this.UseLocalization(app, env);
+         this.UseControllers(app, env);
+      }
+
+   }
+}
