4237623cd06038b90abcd8588e6520eee0b24363
Leverson Carlos <lcjohnny@hotmail.com>
2020-03-01 11:49:40 -0300

    some migrations

--- sources/Migrations/20200220003107_CTL_200219.Designer.cs
+++ sources/Migrations/20200220003107_CTL_200219.Designer.cs
@@ -43,86 +43,6 @@ namespace Collectly.Migrations
                     b.ToTable("collectly_v5_dataCollections");
                 });
 
-            modelBuilder.Entity("Collectly.API.Items.ItemData", b =>
-                {
-                    b.Property<long>("ItemID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bigint")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<long?>("CollectionID")
-                        .HasColumnType("bigint");
-
-                    b.Property<short?>("CollectionSequence")
-                        .HasColumnType("smallint");
-
-                    b.Property<string>("LayoutID")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(450)");
-
-                    b.Property<string>("ResourceID")
-                        .IsRequired()
-                        .HasColumnType("varchar(128)")
-                        .HasMaxLength(128);
-
-                    b.Property<string>("Text")
-                        .IsRequired()
-                        .HasColumnType("varchar(500)")
-                        .HasMaxLength(500);
-
-                    b.HasKey("ItemID");
-
-                    b.HasIndex("CollectionID");
-
-                    b.HasIndex("ResourceID", "LayoutID", "CollectionID")
-                        .HasName("collectly_v5_dataItems_index")
-                        .HasAnnotation("SqlServer:Include", new[] { "ItemID" });
-
-                    b.ToTable("collectly_v5_dataItems");
-                });
-
-            modelBuilder.Entity("Collectly.API.Items.PropertyData", b =>
-                {
-                    b.Property<long>("PropertyID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bigint")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<long>("ItemID")
-                        .HasColumnType("bigint");
-
-                    b.Property<string>("TagID")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(450)");
-
-                    b.Property<bool?>("ValueBoolean")
-                        .HasColumnType("bit");
-
-                    b.Property<DateTime?>("ValueDate")
-                        .HasColumnType("datetime2");
-
-                    b.Property<decimal?>("ValueNumeric")
-                        .HasColumnType("decimal(15,4)");
-
-                    b.Property<string>("ValueText")
-                        .HasColumnType("nvarchar(500)")
-                        .HasMaxLength(500);
-
-                    b.Property<string>("ValueUrl")
-                        .HasColumnType("nvarchar(4000)")
-                        .HasMaxLength(4000);
-
-                    b.HasKey("PropertyID");
-
-                    b.HasIndex("ItemID")
-                        .HasName("collectly_v5_dataProperties_index")
-                        .HasAnnotation("SqlServer:Include", new[] { "PropertyID" });
-
-                    b.HasIndex("TagID");
-
-                    b.ToTable("collectly_v5_dataProperties");
-                });
-
             modelBuilder.Entity("Collectly.API.Layouts.LayoutData", b =>
                 {
                     b.Property<string>("LayoutID")
@@ -165,28 +85,6 @@ namespace Collectly.Migrations
                         .HasAnnotation("SqlServer:Include", new[] { "TagID" });
 
                     b.ToTable("collectly_v5_dataTags");
-                });
-
-            modelBuilder.Entity("Collectly.API.Items.ItemData", b =>
-                {
-                    b.HasOne("Collectly.API.Collections.CollectionData", "CollectionDetails")
-                        .WithMany()
-                        .HasForeignKey("CollectionID");
-                });
-
-            modelBuilder.Entity("Collectly.API.Items.PropertyData", b =>
-                {
-                    b.HasOne("Collectly.API.Items.ItemData", "ItemDetails")
-                        .WithMany("PropertyList")
-                        .HasForeignKey("ItemID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("Collectly.API.Layouts.TagData", "TagDetails")
-                        .WithMany()
-                        .HasForeignKey("TagID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
                 });
 
             modelBuilder.Entity("Collectly.API.Layouts.TagData", b =>
